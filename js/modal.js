68b4780930010592606ac88e5db79ebf5b62c7a9
natalia-ya <sagaidak.nata@gmail.com>
2023-04-23 17:26:43 +0200

    improved modal window

--- js/modal.js
+++ js/modal.js
@@ -1,14 +1,21 @@
 (() => {
   const refs = {
-    openModalBtn: document.querySelector('[data-modal-open]'),
-    closeModalBtn: document.querySelector('[data-modal-close]'),
-    modal: document.querySelector('[data-modal]'),
+    openModalBtn: document.querySelectorAll('[data-modal-open]'),
+    closeModalBtn: document.querySelectorAll('[data-modal-close]'),
+    modal: document.querySelectorAll('[data-modal]'),
   };
 
-  refs.openModalBtn.addEventListener('click', toggleModal);
-  refs.closeModalBtn.addEventListener('click', toggleModal);
+  refs.openModalBtn.forEach(button => {
+    button.addEventListener('click', toggleModal);
+  });
+
+  refs.closeModalBtn.forEach(button => {
+    button.addEventListener('click', toggleModal);
+  });
 
   function toggleModal() {
-    refs.modal.classList.toggle('backdrop--hidden');
+    refs.modal.forEach(modal => {
+      modal.classList.toggle('backdrop--hidden');
+    });
   }
 })();
